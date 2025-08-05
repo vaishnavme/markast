d6c25157bf727b4e1e4121c5b3a435ef99dfcc11
Vaishnav <chandurkarvaishnav@gmail.com>
2025-08-05 19:44:48 +0530

    ref: export Markast class from index.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,5 @@
-import type { TreeType } from "./global.types";
-import JSONTOMarkdown from "./json-to-md";
-import MarkdownToJSON from "./md-to-json";
+import Markast from ".";
 import { testMarkdownString } from "./tests/test-inputs";
-
-class Markast {
-  #mdToJSONParser = new MarkdownToJSON();
-  #JSONToMdParser = new JSONTOMarkdown();
-
-  mdToJSON(string: string) {
-    return this.#mdToJSONParser.parse(string);
-  }
-
-  JSONToMd(tree: TreeType[]) {
-    return this.#JSONToMdParser.parse(tree);
-  }
-}
 
 const markast = new Markast();
 const tree = markast.mdToJSON(testMarkdownString);
